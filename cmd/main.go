cb4a586ba7c0a4037f60fce60ced788dfab13841
Marshall <marshallstutzman@gmail.com>
2024-12-13 08:09:45 -0500

    not complete, need to add read-file feature

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/wowTerrific/HTTPing/ping"
+)
+
+func sendPing(url string, c chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	c <- fmt.Sprintf("%s - %s", url, ping.Ping(url))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// TODO - delete this
+	urls := []string{
+		"https://google.com",
+		"https://youtube.com",
+		"https://facebook.com",
+		"aslfkdjsf",
+	}
+
+	var wg sync.WaitGroup
+	c := make(chan string)
+
+	for _, url := range urls {
+		wg.Add(1)
+		go sendPing(url, c, &wg)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	for resp := range c {
+		if !strings.Contains(resp, "200 OK") {
+			fmt.Printf("%s%s%s\r\n", "\033[31m", resp, "\033[0m")
+		} else {
+			fmt.Println(resp)
+		}
+	}
 }
--- ping/ping.go
+++ ping/ping.go
@@ -1,7 +1,6 @@
 package ping
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,8 +8,8 @@ func Ping(url string) string {
 	res, err := http.Get(url)
 
 	if err != nil {
-		return fmt.Sprintf("%s - %s", url, err.Error())
+		return err.Error()
 	}
 
-	return fmt.Sprintf("%s - %s", url, res.Status)
+	return res.Status
 }
