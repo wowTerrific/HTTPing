c69773d08444ecc946cd81d53d23f10c7b97f5e4
marshall <marshallstutzman@gmail.com>
2024-12-19 07:39:44 -0500

    slow and steady I guess

--- readfile/readfile.go
+++ readfile/readfile.go
@@ -1,1 +1,15 @@
 package readfile
+
+import "strings"
+
+// Read entire file contents into memory with os.ReadFile
+
+func ParseConfig(rawData []byte) ([]string, error) {
+	splitData := strings.Split(strings.Replace(string(rawData), "\r\n", "\n", -1), "\n")
+	return splitData, nil
+}
+
+// need to return line where error exists too
+func ValidateConfig(data []string) (bool, error) {
+	return false, nil
+}
--- readfile/readfile_test.go
+++ readfile/readfile_test.go
@@ -1,1 +1,44 @@
 package readfile
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseConfig(t *testing.T) {
+	data := []byte(`https://www.google.com/
+https://facebook.com/
+https://youtube.com/`)
+	got, err := ParseConfig(data)
+	want := []string{
+		"https://www.google.com/",
+		"https://facebook.com/",
+		"https://youtube.com/",
+	}
+
+	if err != nil {
+		t.Errorf("did not want error, got %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %#v, want %#v", got, want)
+	}
+}
+
+func TestValidateConfig(t *testing.T) {
+	data := []string{
+		"https://www.google.com/",
+		"https://facebook.com/",
+		"https://youtube.com/",
+	}
+	got, err := ValidateConfig(data)
+
+	if err != nil {
+		t.Errorf("should not have recieved error, got %v", err)
+	}
+
+	if !got {
+		t.Errorf("did not recieve ok from config validation")
+	}
+
+}
